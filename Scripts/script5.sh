ecbe2527fc8e6425ae52cfe8a7c710beb9ca38d2
josuered4 <josuered4@gmial.com>
2022-09-24 19:24:10 -0500

    parametros

---
+++ Scripts/script5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#"PARAMENTROS"
+##Cuando de le pasa valores a un scrip o funcion son argumentos 
+##Cuando haces el scrip o funcion son parametros
+
+# ./script5.sh param param2 param3 => es un ejemplo de como ejecutar 
+
+# con la expresion "$#" se retorna el numero de parametros pasados 
+echo "Numero de parametros recividos: $#"
+
+#con la expresion regular "$" segudo de un numero se regresa el numero del parametro
+echo "El parametro 1 es: $1"
+echo "El parametro 2 es: $2"
